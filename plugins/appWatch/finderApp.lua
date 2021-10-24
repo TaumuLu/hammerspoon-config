28f0126798da0c4e99b9eea796d63bbdb3aa87f0
TaumuLu <972409545@qq.com>
2021-10-25 01:20:46 +0800

    feat: finder script add reopenPath action

--- plugins/appWatch/finderApp.lua
+++ plugins/appWatch/finderApp.lua
@@ -45,7 +45,7 @@ local cutAction = hs.hotkey.new({'cmd'}, 'x', function ()
 end)
 
 -- 这里必须设置为全局的函数，否则函数内无法引用到
- PasteAction = hs.hotkey.new({'cmd'}, 'v', function ()
+PasteAction = hs.hotkey.new({'cmd'}, 'v', function ()
   local modifiers = {'cmd'}
   if isCut == true then
     table.insert(modifiers, 'option')
@@ -57,12 +57,60 @@ end)
   PasteAction:enable()
 end)
 
+-- 获取 finder 当前路径
+local getCurrentDir = [[
+tell application "Finder"
+	if exists Finder window 1 then
+		set currentDir to target of Finder window 1 as alias
+	else
+		set currentDir to desktop as alias
+	end if
+end tell
+
+do shell script "echo " & quoted form of POSIX path of currentDir
+]]
+
+local paths = {}
+
+colseAction = hs.hotkey.new({'cmd'}, 'w', function ()
+  colseAction:disable()
+  local success,result,desc = hs.osascript.applescript(getCurrentDir)
+  if success then
+    table.insert(paths, result)
+    Inspect(paths)
+  end
+  hs.eventtap.keyStroke({'cmd'}, 'w')
+  colseAction:enable()
+end)
+
+reopenPath = hs.hotkey.new({'cmd', 'shift'}, 't', function ()
+  reopenPath:disable()
+  local path = table.remove(paths)
+  if path ~= nil then
+    -- 先打开一个新 tab
+    hs.eventtap.keyStroke({'cmd'}, 't')
+    reopenPath:enable()
+    -- 打开文件路径
+    local success = hs.osascript.applescript([[
+      tell application "Finder"
+        set thePath to POSIX file "]]..path..[["
+        set the target of the front Finder window to folder thePath
+      end tell
+    ]])
+  else
+    hs.alert('没有关闭的历史路径')
+    reopenPath:enable()
+  end
+end)
+
 return {
   id = appId,
   enable = function()
     deleteAction:enable()
     cutAction:enable()
     PasteAction:enable()
+    colseAction:enable()
+    reopenPath:enable()
     -- switchTabLeft:enable()
     -- switchTabRight:enable()
   end,
@@ -70,6 +118,8 @@ return {
     deleteAction:disable()
     cutAction:disable()
     PasteAction:disable()
+    colseAction:disable()
+    reopenPath:disable()
     -- switchTabLeft:disable()
     -- switchTabRight:disable()
   end
